9546f9fc904762cda50076f02806ea993f8ff186
MaartenV87 <verheyen.maarten87@gmail.com>
2025-02-02 14:15:46 +0100

    Update app.py

--- app.py
+++ app.py
@@ -34,13 +34,12 @@ catalog_file = st.file_uploader("Upload de Catalogus uit KMOShops (CSV)", type=[
 
 if stock_file and catalog_file:
     if st.button("Filter Stocklijst"):
-        with st.spinner("Bezig met verwerken... Even geduld.."):
+        with st.spinner("Bezig met verwerken..."):
             filtered_df = filter_stock(stock_file, catalog_file)
             
-            # Omzetten naar Excel-bestand
-            output = io.BytesIO()
-            with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
-                filtered_df.to_excel(writer, index=False)
+            # Omzetten naar CSV-bestand
+            output = io.StringIO()
+            filtered_df.to_csv(output, index=False, sep=';')
             output.seek(0)
             
             # Zorg dat datetime correct gebruikt wordt
@@ -50,9 +49,9 @@ if stock_file and catalog_file:
             # Download knop tonen
             st.download_button(
                 label="Download Gefilterde Stocklijst",
-                data=output,
-                file_name=f"Gefilterde_Stocklijst_{current_date}.xlsx",
-                mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+                data=output.getvalue(),
+                file_name=f"Gefilterde_Stocklijst_{current_date}.csv",
+                mime="text/csv"
             )
             
             st.success("De gefilterde stocklijst is succesvol gegenereerd!")
