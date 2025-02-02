d0793641da37b022c635f29df9646111b646d868
MaartenV87 <verheyen.maarten87@gmail.com>
2025-02-02 13:54:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -34,24 +34,25 @@ catalog_file = st.file_uploader("Upload de Catalogus uit KMOShops (CSV)", type=[
 
 if stock_file and catalog_file:
     if st.button("Filter Stocklijst"):
-        filtered_df = filter_stock(stock_file, catalog_file)
-        
-        # Omzetten naar Excel-bestand
-        output = io.BytesIO()
-        with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
-            filtered_df.to_excel(writer, index=False)
-        output.seek(0)
-        
-        # Zorg dat datetime correct gebruikt wordt
-        from datetime import datetime
-        current_date = datetime.now().strftime("%Y-%m-%d")
-        
-        # Download knop tonen
-        st.download_button(
-            label="Download Gefilterde Stocklijst",
-            data=output,
-            file_name=f"Gefilterde_Stocklijst_{current_date}.xlsx",
-            mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-        )
-        
-        st.success("De gefilterde stocklijst is succesvol gegenereerd!")
+        with st.spinner("Bezig met verwerken... Even geduld.."):
+            filtered_df = filter_stock(stock_file, catalog_file)
+            
+            # Omzetten naar Excel-bestand
+            output = io.BytesIO()
+            with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+                filtered_df.to_excel(writer, index=False)
+            output.seek(0)
+            
+            # Zorg dat datetime correct gebruikt wordt
+            from datetime import datetime
+            current_date = datetime.now().strftime("%Y-%m-%d")
+            
+            # Download knop tonen
+            st.download_button(
+                label="Download Gefilterde Stocklijst",
+                data=output,
+                file_name=f"Gefilterde_Stocklijst_{current_date}.xlsx",
+                mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+            )
+            
+            st.success("De gefilterde stocklijst is succesvol gegenereerd!")
